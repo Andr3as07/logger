1e39728f3aeb41ceff89feb3e43d265bcfb4b407
Andreas Evans <andr3as06@gmail.com>
2021-05-20 13:38:25 +0200

    Added a basic console sink

---
+++ Sink/ConsoleSink.cs
@@ -0,0 +1,19 @@
+﻿using Andr3as07.Logging.Formater;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Andr3as07.Logging.Sink {
+  public class ConsoleSink : ISink {
+    public readonly ITextFormater Formater;
+
+    public ConsoleSink(ITextFormater formater) {
+      this.Formater = formater;
+    }
+
+    public void Dispatch(LogLevel level, DateTime time, string message, Dictionary<string, object> context, params object[] data) {
+      Console.WriteLine(Formater.FormatText(level, time, message, context));
+    }
+  }
+}
